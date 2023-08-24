5a1f28e854e9d1caf3c10d34eb45048af16fd8c1
Thomas Dokas <tjdokas@gmail.com>
2023-08-24 21:06:18 +0000

    Add contact front end

--- pennington_photo/js/Contact/src/Contact.jsx
+++ pennington_photo/js/Contact/src/Contact.jsx
@@ -5,17 +5,62 @@ class Contact extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      content: {
+        name: "",
+        email: "",
+        message: "Hello D. Pennington,\n\nI would like to purchase prints of the following photos. Thank you!"
+      }
+    }
+    this.handleChage = this.handleChage.bind(this);
+  }
+
+  handleChage(key, val) {
+    const {
+      content
+    } = this.state;
+    content[key] = val;
+    this.setState({ content });
   }
 
   render() {
+    const {
+      content
+    } = this.state;
+    const {
+      name,
+      email,
+      message
+    } = content;
     return (
       <>
         <NavBar />
         <div className='site-contents'>
-          <div className='dialogue'>
+          <div className='dialogue altbody'>
             <h1>
-              Contact Me
+              Contact Donald N. Pennington
             </h1>
+            <h4>
+              You can email me either via the form below or with your favorite email application at <a href = "mailto: donpennington@comcast.net" className='underline'>donpennington@comcast.net</a>
+            </h4>
+            <form action="/api/v1/contact/" method="post">
+              <label htmlFor="name">Name</label><br />
+              <input type="text" name="name" id="name" value={name} onChange={(e) => { this.handleChage("name", e.target.value) }} required /><br />
+              <br />
+              <label htmlFor="email">email</label><br />
+              <input type="text" name="email" id="email" value={email} onChange={(e) => { this.handleChage("email", e.target.value) }} required /><br />
+              <br />
+              <label htmlFor="message">Message</label><br />
+              <textarea name="message"
+                onChange={(e) => { this.handleChage("message", e.target.value) }}
+                value={message} />
+              <br />
+              <br />
+              <div class="menu-buttons">
+                <button onClick={() => { this.handleChage("name", ""); this.handleChage("email", ""); this.handleChage("message", ""); }}>Cancel</button>
+                <input type="submit" value="Send" />
+              </div>
+            </form>
           </div>
         </div>
       </>
