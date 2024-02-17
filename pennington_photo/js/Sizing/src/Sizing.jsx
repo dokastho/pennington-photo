09ad6bf2b0d6b7f94bb4af2b5ba5974725c6ba81
Thomas Dokas <tjdokas@gmail.com>
2024-02-17 21:54:10 +0000

    Exchange sizing info

--- pennington_photo/js/Sizing/src/Sizing.jsx
+++ pennington_photo/js/Sizing/src/Sizing.jsx
@@ -37,7 +37,7 @@ class Sizing extends React.Component {
           <div className='site-contents'>
             <div className='dialogue'>
               <h1>
-                Pricing
+                Photographics Info
               </h1>
               <div className='p'>
                 <p>
@@ -45,6 +45,9 @@ class Sizing extends React.Component {
                 </p>
                 <p>
                   Prints are signed, numbered, mounted, matted and over-matted on white acid-free 4 ply 100% cotton rag museum board. Framing is silver or black metal. Contact me for additonal details.
+                </p>
+                <p>
+                  Pricing varies per print and can be viewed in your cart.
                 </p>
               </div>
               <h1>
@@ -58,25 +61,20 @@ class Sizing extends React.Component {
                   These photographs are only printed 26" X 32", dry-mounted on 32" X 40" 100% cotton rag museum board, and can be framed on silver or black metal 32" X 40" frames. These exclusive photographic prints are limited to no more than 50 copies each.
                 </p>
                 <h3>
+                  Triptych Prints
+                </h3>
+                <p>
+                  These photographic prints consist of three segments of one image arranged in a traditional triptych format. Two neighboring 6" x 10" photogrpahics accompany one central 10" x 10" print on a 16" x 32" frame. 
+                </p>
+                <h3>
                   Mirror Image Prints
                 </h3>
                 <p>
                   These photographs consist of two prints with one printed in reverse of the other. They are then mounted together on a single 32" X 42" piece of museum board, then over-matted. Framing would consist of silver or black metal.
                 </p>
-                <table>
-                  <tr>
-                    <th>Matted & Framed Size</th>
-                    <th>Matted, Overmatted & Framed Price</th>
-                  </tr>
-                  <tr>
-                    <td>22" x 40"</td>
-                    <td>$1,450</td>
-                  </tr>
-                  <tr>
-                    <td>32" x 40"</td>
-                    <td>$1,750</td>
-                  </tr>
-                </table>
+                <p>
+                  Mirror image prints come in 22" x 40" and 32" x 40" prints.
+                </p>
                 <h3>
                   Select Photographic Prints
                 </h3>
@@ -87,32 +85,22 @@ class Sizing extends React.Component {
                   <tr>
                     <th>Print Size</th>
                     <th>Matted & Framed Size</th>
-                    <th>Matted Price</th>
-                    <th>Matted, Overmatted & Framed Price</th>
                   </tr>
                   <tr>
                     <td>11" X 14"</td>
                     <td>16" X 20"</td>
-                    <td>$300</td>
-                    <td>$450</td>
                   </tr>
                   <tr>
                     <td>16" X 20"</td>
                     <td>28" X 28"</td>
-                    <td>$500</td>
-                    <td>$750</td>
                   </tr>
                   <tr>
                     <td>20" X 24"</td>
                     <td>22" x 40"</td>
-                    <td>$800</td>
-                    <td>$950</td>
                   </tr>
                   <tr>
                     <td>26" X 32"</td>
                     <td>32" X 40"</td>
-                    <td>$1,200</td>
-                    <td>$1,450</td>
                   </tr>
                 </table>
                 <p>
