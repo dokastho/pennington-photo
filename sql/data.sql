473ec6df23b2a69a8b934976415266044e9daa3b
Thomas Dokas <tjdokas@gmail.com>
2024-03-13 20:06:18 +0000

    Update schema for new sizing

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,17 @@
 INSERT INTO users (username, password)
-VALUES ("dokastho", "password");
-INSERT INTO users (username, password)
-VALUES ("donpen", "password");
-INSERT INTO users (username, password)
-VALUES ("janpen", "password");
+VALUES  ("dokastho", "password"),
+        ("donpen", "password"),
+        ("janpen", "password");
+INSERT INTO sizenames (owner, name)
+VALUES  ("donpen", "Matted 11"" x 14"" Print"),
+        ("donpen", "Matted 16"" x 20"" Print"),
+        ("donpen", "Matted 20"" x 24"" Print"),
+        ("donpen", "Matted 26"" x 32"" Print"),
+        ("donpen", "Matted/Overmatted 16"" x 20"" Framed"),
+        ("donpen", "Matted/Overmatted 28"" x 28"" Framed"),
+        ("donpen", "Matted/Overmatted 22"" x 40"" Framed"),
+        ("donpen", "Matted/Overmatted 32"" x 40"" Framed"),
+        ("donpen", "Mirror Image 22"" x 40"" Framed"),
+        ("donpen", "Mirror Image 32"" x 40"" Framed"),
+        ("donpen", "Triptych 16"" x 32"" Framed"),
+        ("donpen", "5”x 7” photographic card available with purchase of print.");
