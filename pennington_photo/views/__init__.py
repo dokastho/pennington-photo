02d18e017b0a0ccb96e791c2c42435c1112e3f30
Thomas Dokas <tjdokas@gmail.com>
2024-05-10 04:58:47 +0000

    Remove sizing from site api

--- pennington_photo/views/__init__.py
+++ pennington_photo/views/__init__.py
@@ -4,4 +4,3 @@ from pennington_photo.views.cart import *
 from pennington_photo.views.contact import *
 from pennington_photo.views.index import *
 from pennington_photo.views.galleries import *
-from pennington_photo.views.sizing import *
