976b4a477474f4a9a8bb6160f86295ba23d5e8fd
TJ Dokas <tjdokas@gmail.com>
2024-06-18 06:16:23 +0000

    Update information on home page

--- pennington_photo/js/Home/src/InfoPage.jsx
+++ pennington_photo/js/Home/src/InfoPage.jsx
@@ -22,13 +22,10 @@ class InfoPage extends React.Component {
         <div className='home-info-content side-by-side'>
           <div className='home-info-text'>
             <p>
-              Visualization and exploration of our natural environment is a rewarding experience. Beauty and composition can be found at arm's reach in our back yard, across the country and around the world. I believe that my next photograph could be my finest. I strive for this concept and continue to cherish for the splendor and magnificence of our natural environment.
+              Visualization and exploration of our natural environment is a rewarding experience. Beauty and composition can be found at arm's reach in our backyard, across the country, and around the world. I believe that my next photograph could be my finest. I strive for this concept and continue to cherish the splendor and magnificence of our natural environment. I believe that B & W photographs emphasize comprehensive visualization of the scene and focuses specifically on the interaction of light as it relates to form, tonality, contrast, mood, texture, shape, composition, and raw emotion within the scene. I believe that a dynamic B & W photograph is a beautiful and timeless image.
             </p>
             <p>
-              To capture the extreme detail, a full range of contrast, and delicate tonalities of the natural scene, my photography is Black & White with an 8 x 10 view camera. I believe that B & W photographs emphasize comprehensive visualization of the scene and focuses specifically on the interaction of light as it relates to form, tonality, contrast, mood, texture, shape, composition, and raw emotion within the scene. I believe that a dynamic B & W photograph is a beautiful and timeless image.
-            </p>
-            <p>
-              All prints are mnade on Silver Gelatin Fiber based photographic paper, processed to archival standards, selenium toned and dry mounted on 100% rag museum board. I personally print and process all prints to order.
+              My images are created using either an 8"x10" or 4"x5" film camera. After the image is exposed, traditional and tedious photographic darkroom processes are incorporated in creating archival silver gelatin prints. Also prints are selenium toned for additional permanence. Processed prints are signed, mounted and over-matted on white acid free 4 ply 100% cotton rag museum board. My ultimate goal is to present exceptional B & W images, inspire viewers to develop a deep appreciation for natural landscapes, and encourage a desire to preserve our physical environment.
             </p>
           </div>
           <img src={`/static/img/${imgName}`} className='home-info-img background-img' />
